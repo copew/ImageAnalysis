fcc977296498254d4f09758748800c75ecf8ad3e
Wei Cope <ww234@login-e-16.data.cluster>
2019-08-27 20:54:01 +0100

    added the tumour lymphocyte relationship analysis and a new corresponding bash scruitp and a testfile for a list of 6 images

---
+++ cellcluster_bashthisscript_new.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+myscriptdir=/home/ww234/cellclusterslurm
+myfilesdir=/rds/user/ww234/hpc-work/itpt
+
+submit=${myscriptdir}/cellcluster_submit.sh
+prepare=${myscriptdir}/cellcluster_prepare.sh
+func=${myscriptdir}/tum_lymph_cluster_hpc.m
+#! images=${myfilesdir}/file_list1.txt #! For real
+images=${myscriptdir}/testfiles.txt #! For debug
+
+
+mkdir outputfiles_tum_lymph
+cd outputfiles_tum_lymph
+while read line ; do sbatch ${submit} ${prepare} ${func} "${myfilesdir}/${line}.fits" ; done<${images}
+#! while read line ; do echo ${submit} ${prepare} ${func} "${myfilesdir}/${line}.fits" ; done<${images} #!For debug
+
+cd ..
